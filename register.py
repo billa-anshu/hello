6b19e7f568b1e45fd7816528b20091943ff25441
billa-anshu <billaanshu0@gmail.com>
2024-09-27 11:12:40 +0530

    Update register.py

--- register.py
+++ register.py
@@ -1,5 +1,26 @@
-try:
-    import tkinter
-    tkinter._test()
-except ImportError:
-    print("Tkinter is not installed")
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# List to store user data
+users = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user = {
+            'name': request.form['name'],
+            'email': request.form['email'],
+            'phone': request.form['phone'],
+            'address': request.form['address']
+        }
+        users.append(user)
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
